f63d2e23debc9c23d0baddb6aab5a5a4e2eeef13
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-06-16 20:18:35 +0300

    feat(hooks): use gofumpt

--- hooks/go-format.sh
+++ hooks/go-format.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-output="$(gofmt -l "$@")"
+output="$(gofumpt -l "$@")"
 
 [[ -z "$output" ]] && exit 0
 
 echo >&2 "Go files must be formatted with gofmt. Please run:"
 for f in $output; do
-  echo >&2 "  gofmt -w $PWD/$f"
+	echo >&2 "  gofumpt -w $PWD/$f"
 done
 
 exit 1
