a0a3c5ade6f4265d71efae4d9880a23357836257
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-06-16 20:40:38 +0300

    fix(hooks): add race flag to gotest

--- hooks/go-test.sh
+++ hooks/go-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if go test -short -v ./... -ne 0; then
-  echo "unit tests failed"
-  exit 1
+if go test -race -v ./... -ne 0; then
+	echo "unit tests failed"
+	exit 1
 fi
